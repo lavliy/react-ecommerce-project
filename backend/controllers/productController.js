9078ce763a76ad0e6442e107771b43e530c048f5
lavliy <natarajanlavanya92@gmail.com>
2025-07-08 17:32:13 -0500

    third commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,25 @@
-exports.getproducts = (req, res, next) => {
+const productModel = require('../models/productModel');
+const ProductModel = require('../models/productModel');
+
+exports.getproducts = async(req, res, next) => {
+
+    const products = await ProductModel.find({});
     res.json({
         sucess: true,
-        message: "Get products working!"
+        products
     })
 }
-exports.getSingleProducts = (req, res, next) => {
-    res.json({
-        sucess: true,
-        message: "Get Single products working!"
-    })
+exports.getSingleProducts = async(req, res, next) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
+            success: true,
+            product
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: 'Unable to get Product with that ID'
+        })
+    }
 }
